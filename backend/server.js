9ed0c54b3f5e34a84f9bbaa7b8045895d09b1867
stepanchaparyan <stepanchaparyan@gmail.com>
2021-01-10 01:45:00 +0400

    Added express routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,35 @@ const express = require('express');
 
 const app = express();
 
+// Routes
+// Users routes
+
+// Register
+app.post('/api/users/register', (req, res) => {
+  res.send('Register route');
+});
+
+// Login
+app.post('/api/users/login', (req, res) => {
+  res.send('Login route');
+});
+
+// Update user
+app.put('/api/users/update', (req, res) => {
+  res.send('Update route');
+});
+
+// Delete user
+app.delete('/api/users/:id', (req, res) => {
+  res.send('Delete route');
+});
+
+// Fetch users
+app.get('/api/users', (req, res) => {
+  res.send('Fetch route');
+});
+
+// Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {

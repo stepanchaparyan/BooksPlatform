03fee5aacec4557a8687b11e7006817b4900b631
stepanchaparyan <stepanchaparyan@gmail.com>
2021-01-14 01:03:22 +0400

    Added front - react, redux

---
+++ frontend/src/redux/reducers/books/createBookReducer.js
@@ -0,0 +1,26 @@
+import { CREATE_BOOK_REQUEST, CREATE_BOOK_SUCCESS, CREATE_BOOK_FAIL } from '../../actions/actionsTypes';
+
+const createBookReducer = (state={}, action) => {
+  const { type, payload } = action;
+  switch(type) {
+    case CREATE_BOOK_REQUEST:
+      return {
+        loading: true,
+      };
+    case CREATE_BOOK_SUCCESS:
+      return {
+        loading: false,
+        book: payload,
+      };
+    case CREATE_BOOK_FAIL:
+      return {
+        loading: false,
+        error: payload,
+      };
+
+    default: 
+      return state; 
+  };
+};
+
+export {createBookReducer};
